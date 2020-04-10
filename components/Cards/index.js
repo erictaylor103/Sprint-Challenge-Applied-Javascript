817492130ff7e5a60e621061a87351092140f989
Eric Taylor <erictaylor@Erics-MacBook-Pro.local>
2020-04-10 15:15:16 -0400

    Commit of the completed Cards/index.js file task with personal notes

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -27,10 +27,48 @@
 
 axios.get("https://lambda-times-backend.herokuapp.com/articles")
 .then(res =>{
-  //console.log(res.data);
+  //console.log(res.data); // checked to see if data results are being fetched correctly by the axios function
+
+  const articlesData = res.data.articles; //created variable to save all the articles data results so I can use it on my forEach loops in a Parent - Child manner
+  //console.log(articlesData);
+
+  //checked to see if each specific article could be retrieved from my articlesData variable in which all the article data results is saved
+  //console.log(articles.javascript);
+  //console.log(articles.bootstrap);
+  //console.log(articles.technology);
+  //console.log(articles.jquery);
+  //console.log(articles.node);
+  const allCards  = document.querySelector(".cards-container"); // created variable to store the MAIN CARDS container by selecting the cards-container class in the HTML
+
+  //CREATE LOOPS - these will select each specific article and append the createCards function to allCards variable that has the ".cards-container" div stored
+
+  articlesData.javascript.forEach(card =>{
+    allCards.appendChild(createCards(card.headline, card.authorPhoto, card.authorName));
+  });
+
+  articlesData.bootstrap.forEach(card =>{
+    allCards.appendChild(createCards(card.headline, card.authorPhoto, card.authorName));
+  });
+
+  articlesData.technology.forEach(card =>{
+    allCards.appendChild(createCards(card.headline, card.authorPhoto, card.authorName));
+  });
+
+  articlesData.jquery.forEach(card =>{
+    allCards.appendChild(createCards(card.headline, card.authorPhoto, card.authorName));
+  });
+
+  articlesData.node.forEach(card =>{
+    allCards.appendChild(createCards(card.headline, card.authorPhoto, card.authorName));
+  });
+
 })//end .then
+.catch(error =>{
+  console.log(error);
+})
 
 function createCards(headline, authorPhoto, authorName){
+
   //CREATING THE ELEMENTS FOR THE HTML PAGE
   const cardDiv         = document.createElement("div");
   const headlineDiv     = document.createElement("div");
@@ -46,36 +84,21 @@ function createCards(headline, authorPhoto, authorName){
   imgDivContainer.appendChild(authorImage);
   authorDiv.appendChild(authorNameSpan);
 
+  //CREATE/ADD THE CLASSES
+  cardDiv.classList.add("card");
+  headlineDiv.classList.add("headline");
+  authorDiv.classList.add("author");
+  imgDivContainer.classList.add("img-container");
+
+  //SET TEXT CONTENT and SRC
+  headlineDiv.textContent = headline;
+  authorImage.setAttribute("src", authorPhoto);
+  authorNameSpan.textContent = "By: " + authorName;
 
 
 
-  console.log(cardDiv);
+  return cardDiv;
+
 }
-createCards();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 //END OF FILE
