e5581d6d49977a02fc3e037e7939c8a5a2fe0e54
Eric Taylor <erictaylor@Erics-MacBook-Pro.local>
2020-04-10 11:48:11 -0400

    Completed the Header/index.js file task

--- components/Header/index.js
+++ components/Header/index.js
@@ -3,7 +3,7 @@
 // Write a function that returns the markup you see below:
 //
 //  <div class="header">
-//    <span class="date">SMARCH 28, 2019</span>
+//    <span class="date">MARCH 28, 2019</span>
 //    <h1>Lambda Times</h1>
 //    <span class="temp">98°</span>
 //  </div>
@@ -11,4 +11,45 @@
 // Use your function to create a header
 // and append it to the DOM inside the div.header-container
 
-function Header() {}
+function Header() {
+  //CREATING THE COMPONENTS FOR THE HTML
+  const headerContainer = document.querySelector(".header-container");
+
+  const headerDiv       = document.createElement("div");
+  const date            = document.createElement("span");
+  const title           = document.createElement("h1");
+  const temperature     = document.createElement("span");
+
+  //STRUCTURE
+  headerDiv.appendChild(date);
+  headerDiv.appendChild(title);
+  headerDiv.appendChild(temperature);
+  headerContainer.appendChild(headerDiv);
+
+  //CREATE THE CLASSES
+  headerDiv.classList.add("header");
+  date.classList.add("date");
+  temperature.classList.add("temp");
+
+  //ADD THE TEXT Content
+  date.textContent        = "MARCH 28, 2019";
+  title.textContent       = "Lambda Times";
+  temperature.textContent = "98°";
+
+  return headerDiv;
+}
+Header();
+
+
+
+
+
+
+
+
+
+
+
+
+
+//END OF FILE
