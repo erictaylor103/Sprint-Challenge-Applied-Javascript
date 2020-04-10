55ea084b284d94ec050396c4bd6773c38aa1b2f1
Eric Taylor <erictaylor@Erics-MacBook-Pro.local>
2020-04-10 13:59:53 -0400

    COmmit of the unfinished Cards/index.js file task - Requested more time for the Sprint

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -8,13 +8,74 @@
 // Write a function that returns the following markup:
 //
 // <div class="card">
+
 //   <div class="headline">{Headline of article}</div>
+
 //   <div class="author">
+//
 //     <div class="img-container">
 //       <img src={url of authors image} />
 //     </div>
-//     <span>By {author's name}</span>
+//
+//      <span>By {author's name}</span>
+//
 //   </div>
+
 // </div>
 //
 // Use your function to create a card for each of the articles and add the card to the DOM.
+
+axios.get("https://lambda-times-backend.herokuapp.com/articles")
+.then(res =>{
+  //console.log(res.data);
+})//end .then
+
+function createCards(headline, authorPhoto, authorName){
+  //CREATING THE ELEMENTS FOR THE HTML PAGE
+  const cardDiv         = document.createElement("div");
+  const headlineDiv     = document.createElement("div");
+  const authorDiv       = document.createElement("div");
+  const imgDivContainer = document.createElement("div");
+  const authorImage     = document.createElement("img");
+  const authorNameSpan  = document.createElement("span");
+
+  //STRUCTURE
+  cardDiv.appendChild(headlineDiv);
+  cardDiv.appendChild(authorDiv);
+  authorDiv.appendChild(imgDivContainer);
+  imgDivContainer.appendChild(authorImage);
+  authorDiv.appendChild(authorNameSpan);
+
+
+
+
+  console.log(cardDiv);
+}
+createCards();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//END OF FILE
